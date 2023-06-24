ef44b0f464b7ff86f399fd21e09df2761f5a7d14
Adomelism <adomelism@gmail.com>
2023-06-24 06:08:58 +0300

    commit to fix una to install axios

--- src/Pages/AuthorPage/AuthorPage.js
+++ src/Pages/AuthorPage/AuthorPage.js
@@ -1,7 +1,19 @@
 import Container from '../Components/Container/Container';
+import { useEffect, useState } from 'react';
+import { API_URL } from '../../config';
+import { v4 as uuid } from 'uuid';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
+
 
 
 const AuthorPage = () => {
+
+  const { id } = useParams();
+  console.log(id)
+
+
   return (
     <Container>
 
--- src/Pages/AuthorsPage/AuthorsPage.js
+++ src/Pages/AuthorsPage/AuthorsPage.js
@@ -1,11 +1,30 @@
 import Container from '../Components/Container/Container';
-
+import { useEffect, useState } from 'react';
+import { API_URL } from '../../config';
+import { Link } from 'react-router-dom';
 
 const AuthorsPage = () => {
+
+  const [authors, setAuthors] = useState([])
+
+  useEffect(() => {
+    fetch(`${API_URL}/authors`)
+        .then(res => res.json())
+        .then(data => {
+          setAuthors(data)        
+    })
+}, [])
+
   return (
     <Container>
-
-      <div>AuthorsPage</div>
+      <Link to='/authors/create'>Add a New Author</Link>
+        <h1>Authors:</h1>
+        <ul>
+            {authors.map(author => (
+                <li key={author.id}>
+                 <Link to ={`/author/${author.id}`}>{author.name}</Link> {author.nationality} citizen, born in {author.born} with estimated net worth of {author.netWorth}.
+                </li>))} 
+        </ul>
     </Container>
   )
 }

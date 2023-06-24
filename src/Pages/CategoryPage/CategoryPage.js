322292284662f16fa8f45e270c51e368ac481273
Adomelism <adomelism@gmail.com>
2023-06-24 08:19:39 +0300

    commit for the day needs categorypage fix

--- src/Pages/CategoryPage/CategoryPage.js
+++ src/Pages/CategoryPage/CategoryPage.js
@@ -1,10 +1,35 @@
 import Container from '../Components/Container/Container';
+import { Link, useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { API_URL } from '../../config';
+import axios from 'axios';
 
 const CategoryPage = () => {
+
+  const { id } = useParams();
+
+  const [category, setCategory] = useState(null)
+
+  useEffect(() => {
+    fetch(`${API_URL}/books?_expand=category`)
+    .then(res => res.json())
+    .then(data => {
+      setCategory(data)
+    })
+  }, [id])
+
+  if (!category) {
+    return '';
+  }
+
+// console.log(category.category.genre)
+// console.log(category.title)
+// axios.get(`${API_URL}/books?_expand=category`)
+
   return (
     <Container>
 
-      <div>CategoryPage</div>
+    
     </Container>
   )
 }
